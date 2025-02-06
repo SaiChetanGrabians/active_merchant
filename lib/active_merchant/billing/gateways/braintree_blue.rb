ef3a3bc1f1fdfc559ea4da1bb31d930c3b030a3b
SaiChetanGrabians <sai.c@grabians.com>
2025-02-06 13:30:42 +0000

    Update braintree min sdk version from 2.78 to 4.23

--- lib/active_merchant/billing/gateways/braintree_blue.rb
+++ lib/active_merchant/billing/gateways/braintree_blue.rb
@@ -8,7 +8,7 @@ rescue LoadError
   raise 'Could not load the braintree gem.  Use `gem install braintree` to install it.'
 end
 
-raise 'Need braintree gem >= 2.0.0.' unless Braintree::Version::Major >= 2 && Braintree::Version::Minor >= 0
+raise "Need braintree gem >= 4.23.0. Run `gem install braintree --version '~>4.23'` to get the correct version." unless Braintree::Version::Major == 4 && Braintree::Version::Minor >= 23
 
 module ActiveMerchant # :nodoc:
   module Billing # :nodoc:
